a7d47fb4171e7192467be06841d89c5eba01edd3
Wenqiang-Hu <63572326+Wenqiang-Hu@users.noreply.github.com>
2022-10-22 16:07:17 -0700

    At your service activity..

--- app/src/main/java/edu/northeastern/cs5520fa22groupproject/MainActivity.java
+++ app/src/main/java/edu/northeastern/cs5520fa22groupproject/MainActivity.java
@@ -2,7 +2,10 @@ package edu.northeastern.cs5520fa22groupproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button atYourService = findViewById((R.id.atyourservice));
+        atYourService.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent clicky = new Intent(MainActivity.this, AtYourService.class);
+                startActivity(clicky);
+            }
+        });
     }
 }
