5bf29f413132f44e20495ceef9325e48b0e7da47
Yanling <huang.yanl@northeastern.edu>
2022-11-04 17:42:46 -0700

    Get started with setting up firebase authentification
    Yanling, Zhixiang, Kexin, Wenqiang all contributed in sync programming.

--- app/src/main/java/edu/northeastern/cs5520fa22groupproject/MainActivity.java
+++ app/src/main/java/edu/northeastern/cs5520fa22groupproject/MainActivity.java
@@ -7,20 +7,36 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
 
         Button atYourService = findViewById((R.id.atyourservice));
-        atYourService.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view) {
-                Intent clicky = new Intent(MainActivity.this, AtYourService.class);
-                startActivity(clicky);
-            }
-        });
+        atYourService.setOnClickListener(this);
+
+        Button stickButton = findViewById(R.id.button_Stick_It);
+        stickButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int theId = v.getId();
+        if (theId == R.id.atyourservice) {
+            Intent clicky = new Intent(MainActivity.this, AtYourService.class);
+            startActivity(clicky);
+        } else if (theId == R.id.button_Stick_It) {
+            Intent intent = new Intent(this, Sticker.class);
+            startActivity(intent);
+        }
     }
 }
