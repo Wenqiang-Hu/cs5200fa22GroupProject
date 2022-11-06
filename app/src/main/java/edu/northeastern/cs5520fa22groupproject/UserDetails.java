c061010ac0539108030b915d3fc45c783e5cf953
Kexin Qiu <kristenqiu19@gmail.con>
2022-11-06 08:58:53 -0500

    user method

--- app/src/main/java/edu/northeastern/cs5520fa22groupproject/UserDetails.java
+++ app/src/main/java/edu/northeastern/cs5520fa22groupproject/UserDetails.java
@@ -10,4 +10,28 @@ public class UserDetails {
         this.id = id;
         this.username = username;
     }
+
+    public static String getUsername() {
+        return username;
+    }
+
+    public static void setUsername(String username) {
+        UserDetails.username = username;
+    }
+
+    public static String getChatWith() {
+        return chatWith;
+    }
+
+    public static void setChatWith(String chatWith) {
+        UserDetails.chatWith = chatWith;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 }
