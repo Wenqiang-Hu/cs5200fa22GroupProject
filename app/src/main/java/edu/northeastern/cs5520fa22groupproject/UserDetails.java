ec1f79f351209e749d457f21914c23a13248c717
Kexin Qiu <kristenqiu19@gmail.con>
2022-11-06 01:08:03 -0500

    user model

--- app/src/main/java/edu/northeastern/cs5520fa22groupproject/UserDetails.java
+++ app/src/main/java/edu/northeastern/cs5520fa22groupproject/UserDetails.java
@@ -3,4 +3,11 @@ package edu.northeastern.cs5520fa22groupproject;
 public class UserDetails {
     static String username = "";
     static String chatWith = "";
+
+    private String id;
+
+    public UserDetails(String username, String id) {
+        this.id = id;
+        this.username = username;
+    }
 }
