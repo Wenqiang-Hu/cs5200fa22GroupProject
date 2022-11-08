9b1bfef65c8989dee0a38db3a1db4dc5a284cc40
Wenqiang-Hu <63572326+Wenqiang-Hu@users.noreply.github.com>
2022-11-07 22:30:51 -0800

    Yanling, Zhixiang, Kexin, Wenqiang all contributed in sync programming.
    Reference link:
    https://www.youtube.com/watch?v=9JdbgoYgCyA&ab_channel=CodingZest

--- app/src/main/java/edu/northeastern/cs5520fa22groupproject/RegisterActivity.java
+++ app/src/main/java/edu/northeastern/cs5520fa22groupproject/RegisterActivity.java
@@ -35,11 +35,6 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-//        Toolbar toolbar = findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
-//        getSupportActionBar().setTitle("Register");
-
-
         username = findViewById(R.id.username);
         email = findViewById(R.id.email);
         password = findViewById(R.id.password);
