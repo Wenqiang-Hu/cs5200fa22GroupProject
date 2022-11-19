98dd86001a3200cd12ef233519880f2d482b35b2
Yanling <huang.yanl@northeastern.edu>
2022-11-19 11:11:03 -0800

    correct bug when open chatroom
    Yanling Huang, Zhixiang Huo, Wenqiang Hu, Kexin Qiu all contributed

--- app/src/main/java/edu/northeastern/cs5520fa22groupproject/EasyLifeMessageActivity.java
+++ app/src/main/java/edu/northeastern/cs5520fa22groupproject/EasyLifeMessageActivity.java
@@ -2,10 +2,12 @@ package edu.northeastern.cs5520fa22groupproject;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -32,7 +34,7 @@ public class EasyLifeMessageActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_easy_life_chat_message);
 
-//        Toolbar toolbar = findViewById(R.id.toolbar);
+//        Toolbar toolbar = findViewById(R.id.toolbar_easylife);
 //        setSupportActionBar(toolbar);
 //        getSupportActionBar().setTitle("");
 //        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -41,7 +43,7 @@ public class EasyLifeMessageActivity extends AppCompatActivity {
 //            public void onClick(View view) {
 //                finish();
 //                // and this
-//                //startActivity(new Intent(MessageActivity.this, MainActivity.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
+//                startActivity(new Intent(EasyLifeMessageActivity.this, EasyLifeMainActivity.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
 //            }
 //        });
 
--- app/src/main/java/edu/northeastern/cs5520fa22groupproject/model/EasyLifeChatroom.java
+++ app/src/main/java/edu/northeastern/cs5520fa22groupproject/model/EasyLifeChatroom.java
@@ -13,6 +13,9 @@ public class EasyLifeChatroom {
         this.roomname = roomname;
     }
 
+    public EasyLifeChatroom() {
+    }
+
     public String getId() {
         return id;
     }
