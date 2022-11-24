73b398cece266d3ced9614cd6067c816e22317e0
Kexin Qiu <kristenqiu19@gmail.con>
2022-11-24 03:00:08 -0500

    change profile image

--- app/src/main/java/edu/northeastern/cs5520fa22groupproject/Sticker.java
+++ app/src/main/java/edu/northeastern/cs5520fa22groupproject/Sticker.java
@@ -4,11 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import com.firebase.client.Firebase;
+//import com.firebase.client.Firebase;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.analytics.FirebaseAnalytics;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.ktx.Firebase;
 
 public class Sticker extends AppCompatActivity {
 
@@ -18,15 +19,15 @@ public class Sticker extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        Firebase.setAndroidContext(this);
-        setContentView(R.layout.activity_sticker);
-
-        FirebaseApp.initializeApp(this);
-        // Obtain the FirebaseAnalytics instance.
-        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
-        // Initialize Firebase Auth
-        mAuth = FirebaseAuth.getInstance();
+//        super.onCreate(savedInstanceState);
+//
+//        Firebase.setAndroidContext(this);
+//        setContentView(R.layout.activity_sticker);
+//
+//        FirebaseApp.initializeApp(this);
+//        // Obtain the FirebaseAnalytics instance.
+//        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+//        // Initialize Firebase Auth
+//        mAuth = FirebaseAuth.getInstance();
     }
 }
